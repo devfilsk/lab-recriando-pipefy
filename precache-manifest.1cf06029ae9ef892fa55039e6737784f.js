8dcc5ec5615a1bb5bd7a89e616a9d287762ad685
Filipe Maciel <devfilsk@gmail.com>
2019-08-18 12:48:24 -0300

    Updates

---
+++ precache-manifest.1cf06029ae9ef892fa55039e6737784f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b979cd58458d5b54a8ffffa716729060",
+    "url": "/lab-recriando-pipefy/index.html"
+  },
+  {
+    "revision": "8961d04551f0b906473c",
+    "url": "/lab-recriando-pipefy/static/js/2.9a0ae0a1.chunk.js"
+  },
+  {
+    "revision": "65fedb90f8ac56d22993",
+    "url": "/lab-recriando-pipefy/static/js/main.744f694c.chunk.js"
+  },
+  {
+    "revision": "91493a39faae09b0c17d",
+    "url": "/lab-recriando-pipefy/static/js/runtime~main.9eabcf96.js"
+  }
+]);
